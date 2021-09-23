fac911b7301a4b294b7bb87a5ce74b3614517e1d
ESCG\526737 <526737@chrome.escg.ac.uk>
2021-09-23 14:50:06 +0100

    first commit with variables and list

---
+++ Hello Word 2/Hello Word 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hello_Word_2
+{
+    class Program
+    {
+        static int age = 16;
+        static float age2 = 16.2f;
+        static string word = "My age is ";
+        static string word2 = "My exact age is ";
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            List<string> list2 = new List<string>();
+            list2.Add("test1");
+            list2.Add("test2");
+            list2.Add("test3");
+            list2.Add("test4");
+            list2.Add("test5");
+
+            foreach (string i in list2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(word + age);
+            Console.WriteLine();
+            Console.WriteLine(word2 + age2);
+
+            Console.ReadKey();
+        }
+
+    }
+}
